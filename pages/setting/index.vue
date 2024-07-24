85375e9df1652cec2d5e5f610dca99647a3311a5
xieyibo <1226594277@qq.com>
2024-07-24 14:23:08 +0800

    feat：添加半自动校准偏差值

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -4,7 +4,7 @@
 		height: 18px;
 		border-radius: 10px;
 		margin-right: 6px;
-		background-color: #e43d33;
+		background-color: #e64340;
 	}
 
 	.greenDecoration {
@@ -21,6 +21,37 @@
 		border-radius: 10px;
 		margin-right: 6px;
 		background-color: #ffaa00;
+	}
+
+	.timeCalibration {
+		width: 4px;
+		height: 32px;
+		border-radius: 10px;
+		margin-right: 6px;
+		background-color: #2979ff;
+	}
+
+	.timeCalibration_content {
+		padding: 15px;
+	}
+
+	.timeCalibration_button_red {
+		margin: 0px 8px 0px 6px;
+	}
+
+	.timeCalibration_button_green {
+		margin: 0px 6px 0px 8px;
+		color: #ffffff;
+		background-color: #18bc37;
+	}
+
+	.timeCalibration_button_green_hover {
+		color: rgb(255, 255, 255, 0.6);
+		background-color: #00aa00;
+	}
+
+	.demo-uni-row {
+		padding-bottom: 12px;
 	}
 </style>
 <template>
@@ -48,6 +79,46 @@
 			<uni-easyinput type="number" trim="all" v-model="misregistration" :clearable="false"
 				@input="misregistrationInput"></uni-easyinput>
 		</uni-section>
+
+		<uni-collapse>
+			<uni-collapse-item>
+				<template v-slot:title>
+					<uni-section title="校准时间" subTitle="半自动校准时间" titleFontSize="18px">
+						<template v-slot:decoration>
+							<view class="timeCalibration"></view>
+						</template>
+					</uni-section>
+				</template>
+				<view class="timeCalibration_content">
+					<uni-row class="demo-uni-row">
+						<uni-col :span="12">
+							<button class="timeCalibration_button_red" type="warn"
+								@click="quickCalibration(redLight)">按红灯校准</button>
+						</uni-col>
+						<uni-col :span="12">
+							<button class="timeCalibration_button_green"
+								hover-class="timeCalibration_button_green_hover"
+								@click="quickCalibration(0)">按绿灯校准</button>
+						</uni-col>
+					</uni-row>
+					<uni-row class="demo-uni-row">
+						<uni-col :span="24">
+							<button class="timeCalibration_button_green"
+								hover-class="timeCalibration_button_green_hover"
+								@click="quickCalibration(-15)">按绿灯校准（45秒）</button>
+						</uni-col>
+					</uni-row>
+				</view>
+			</uni-collapse-item>
+		</uni-collapse>
+
+
+		<view>
+			<!-- 提示信息弹窗 -->
+			<uni-popup ref="message" type="message">
+				<uni-popup-message :type="msgType" :message="messageText" :duration="2000"></uni-popup-message>
+			</uni-popup>
+		</view>
 	</view>
 </template>
 
@@ -56,6 +127,8 @@
 	export default {
 		data() {
 			return {
+				type: "success",
+				message: "修改成功",
 				redLight: store.state.countdown.redLight,
 				greenLight: store.state.countdown.greenLight,
 				misregistration: store.state.countdown.misregistration
@@ -63,7 +136,6 @@
 		},
 		methods: {
 			redLightInput(countdown) {
-				console.log(store.state.countdown)
 				store.state.countdown.redLight = parseInt(countdown);
 				store.commit("setCountdown", store.state.countdown);
 				this.updateKV();
@@ -78,6 +150,11 @@
 				store.commit("setCountdown", store.state.countdown);
 				this.updateKV();
 			},
+			messageToggle(type = "success", messageText = "修改成功") {
+				this.msgType = type;
+				this.messageText = messageText;
+				this.$refs.message.open();
+			},
 			updateKV() {
 				uni.request({
 					url: 'https://tscmonitorkv.msktmi.com/put',
@@ -89,11 +166,37 @@
 						key: 'countdown',
 						value: JSON.stringify(store.state.countdown)
 					},
+					fail: (res) => {
+						this.messageToggle("error", "保存失败，请检查网络连接");
+					},
 					success: (res) => {
-						//res.data == "True"
+						this.messageToggle();
 					}
 				});
 			},
+			quickCalibration(offset) {
+				const epoch = new Date();
+				epoch.setHours(0, 0, 0, 0);
+				epoch.setDate(epoch.getDate() - 1);
+				epoch.setSeconds(0);
+				var interval = store.getters.interval;
+				const offsetTime = Date.now() - epoch.getTime();
+				const second = (offsetTime / 1000) % interval;
+
+				var misregistration = second + offset;
+				while (misregistration < 0) {
+					misregistration += interval;
+				}
+				while (misregistration > interval) {
+					misregistration -= interval;
+				}
+
+				//保存偏移量
+				this.misregistration = parseInt(misregistration);
+				store.state.countdown.misregistration = parseInt(misregistration);
+				this.updateKV();
+			},
+
 		}
 	}
 </script>
